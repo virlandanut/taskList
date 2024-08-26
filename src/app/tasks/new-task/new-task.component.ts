4bab86123c5818d066b83e21cdf3396182df23ef
Vîrlan Dănuț-Ionel <virlan.danut@outlook.com>
2024-08-26 11:09:24 +0300

    Add files via upload

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDate: string = '';
+  private tasksService = inject(TasksService);
+
+  onSubmit() {
+    this.tasksService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        date: this.enteredDate,
+      },
+      this.userId
+    );
+    this.close.emit();
+  }
+
+  onCancel() {
+    this.close.emit();
+  }
+}
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTask: boolean = false;
+
+  constructor(private tasksService: TasksService) {}
+
+  get selectedUserTask() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+}
